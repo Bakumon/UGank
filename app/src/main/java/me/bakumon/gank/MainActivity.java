d3246f8b8ed38b9415e4afdf066857d3daf004e9
Bakumon <ma_fei@aliyun.com>
2016-11-30 23:17:44 +0800

    add tumblr style fab

--- app/src/main/java/me/bakumon/gank/MainActivity.java
+++ app/src/main/java/me/bakumon/gank/MainActivity.java
@@ -1,15 +1,27 @@
 package me.bakumon.gank;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.melnykov.fab.FloatingActionButton;
+import com.tiancaicc.springfloatingactionmenu.MenuItemView;
+import com.tiancaicc.springfloatingactionmenu.OnMenuActionListener;
+import com.tiancaicc.springfloatingactionmenu.SpringFloatingActionMenu;
+
+import java.lang.reflect.Field;
 
 import me.bakumon.gank.adapter.CommonViewPagerAdapter;
 import me.bakumon.gank.fragment.MyFragment;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TabLayout mTl;
     private ViewPager mVp;
@@ -19,6 +31,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+    }
+
+    private void fab() {
+        // create your own FAB
+        // 必须手动创建FAB, 并设置属性
+        final FloatingActionButton fab = new FloatingActionButton(this);
+        fab.setType(FloatingActionButton.TYPE_NORMAL);
+//        fab.setImageResource(icon);
+        fab.setColorPressedResId(R.color.colorPrimary);
+        fab.setColorNormalResId(R.color.fab);
+        fab.setColorRippleResId(R.color.text_color);
+        fab.setShadow(true);
+
+        new SpringFloatingActionMenu.Builder(this)
+                .fab(fab)
+                //add menu item via addMenuItem(bgColor,icon,label,label color,onClickListener)
+                //添加菜单按钮参数依次是背景颜色,图标,标签,标签的颜色,点击事件
+                .addMenuItem(R.color.photo, R.mipmap.ic_messaging_posttype_photo, "Photo", R.color.text_color,this)
+                .addMenuItem(R.color.chat, R.mipmap.ic_messaging_posttype_chat, "Chat", R.color.text_color,this)
+                .addMenuItem(R.color.quote, R.mipmap.ic_messaging_posttype_quote, "Quote", R.color.text_color,this)
+                .addMenuItem(R.color.link, R.mipmap.ic_messaging_posttype_link, "Link", R.color.text_color,this)
+                .addMenuItem(R.color.audio, R.mipmap.ic_messaging_posttype_audio, "Audio", R.color.text_color,this)
+                .addMenuItem(R.color.text, R.mipmap.ic_messaging_posttype_text, "Text", R.color.text_color,this)
+                .addMenuItem(R.color.video, R.mipmap.ic_messaging_posttype_video, "Video", R.color.text_color,this)
+                //you can choose menu layout animation
+                //设置动画类型
+                .animationType(SpringFloatingActionMenu.ANIMATION_TYPE_TUMBLR)
+                //setup reveal color while the menu opening
+                //设置reveal效果的颜色
+                .revealColor(R.color.colorPrimary)
+                //set FAB location, only support bottom center and bottom right
+                //设置FAB的位置,只支持底部居中和右下角的位置
+                .gravity(Gravity.RIGHT | Gravity.BOTTOM)
+                .onMenuActionListner(new OnMenuActionListener() {
+                    @Override
+                    public void onMenuOpen() {
+                        //set FAB icon when the menu opened
+                        //设置FAB的icon当菜单打开的时候
+//                        fab.setImageResource(icon_closed);
+                    }
+
+                    @Override
+                    public void onMenuClose() {
+                        //set back FAB icon when the menu closed
+                        //设置回FAB的图标当菜单关闭的时候
+//                        fab.setImageResource(icon_opend);
+                    }
+                })
+                .build();
     }
 
     private void initView() {
@@ -43,11 +104,50 @@ public class MainActivity extends AppCompatActivity {
         infoPagerAdapter.addFragment(myFragment4);
         infoPagerAdapter.addFragment(myFragment5);
 
+
         mVp.setAdapter(infoPagerAdapter);
-        mTl.setupWithViewPager(mVp);
+        mTl.setupWithViewPager(mVp);setUpIndicatorWidth();
         mTl.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        mVp.setCurrentItem(0);
+        mVp.setCurrentItem(1);
+    }
+    /**
+     * 通过反射修改TabLayout Indicator的宽度（仅在Android 4.2及以上生效）
+     */
+    private void setUpIndicatorWidth() {
+        Class<?> tabLayoutClass = mTl.getClass();
+        Field tabStrip = null;
+        try {
+            tabStrip = tabLayoutClass.getDeclaredField("mTabStrip");
+            tabStrip.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        LinearLayout layout = null;
+        try {
+            if (tabStrip != null) {
+                layout = (LinearLayout) tabStrip.get(mTl);
+            }
+            for (int i = 0; i < layout.getChildCount(); i++) {
+                View child = layout.getChildAt(i);
+                child.setPadding(0, 0, 0, 0);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                    params.setMarginStart(5);
+                    params.setMarginEnd(5);
+                }
+                child.setLayoutParams(params);
+                child.invalidate();
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
+    public void onClick(View view) {
+        MenuItemView menuItemView = (MenuItemView) view;
+        Toast.makeText(this,menuItemView.getLabelTextView().getText(),Toast.LENGTH_SHORT).show();
+    }
 }
