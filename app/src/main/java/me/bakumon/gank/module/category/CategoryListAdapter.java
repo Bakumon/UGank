1b55c63de6c8e4cf6cb1318ed09b5b24b0d2b4e6
Bakumon <ma_fei@aliyun.com>
2016-12-13 21:41:28 +0800

    toolbar

--- app/src/main/java/me/bakumon/gank/module/category/CategoryListAdapter.java
+++ app/src/main/java/me/bakumon/gank/module/category/CategoryListAdapter.java
@@ -31,11 +31,13 @@ public class CategoryListAdapter extends CommonAdapter4RecyclerView<CategoryResu
     public void convert(CommonHolder4RecyclerView holder, CategoryResult.ResultsBean androidResult) {
         if (androidResult != null) {
             ImageView imageView = holder.getView(R.id.iv_item_img);
-            if (androidResult.images != null && androidResult.images.size() > 0) {
-                GlideUtil.showImgIfGIf(mContext, androidResult.images.get(0) + "?imageView2/0/w/400", imageView);
-            } else {
-                Glide.with(mContext).load(R.mipmap.image_default).into(imageView);
-            }
+            imageView.setVisibility(View.GONE);
+            // TODO: 16-12-13 图片是否加载
+//            if (androidResult.images != null && androidResult.images.size() > 0) {
+//                GlideUtil.showImgIfGIf(mContext, androidResult.images.get(0) + "?imageView2/0/w/400", imageView);
+//            } else {
+//                Glide.with(mContext).load(R.mipmap.image_default).into(imageView);
+//            }
             holder.setTextViewText(R.id.tv_item_title, androidResult.desc == null ? "unknown" : androidResult.desc);
             holder.setTextViewText(R.id.tv_item_publisher, androidResult.who == null ? "unknown" : androidResult.who);
             holder.setTextViewText(R.id.tv_item_time, DateUtil.dateFormat(androidResult.publishedAt));
